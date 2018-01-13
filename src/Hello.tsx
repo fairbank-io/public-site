f02e98a696f78082c5a411461c822ce030287c1b
Ben Cessa <ben@pixative.com>
2018-01-13 18:51:12 -0500

    Cleanup test files

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,26 +1,18 @@
 import * as React from 'react';
 
 export interface Props {
-  name: string;
+  message: string;
   enthusiasmLevel?: number;
-  onIncrement?: () => void;
-  onDecrement?: () => void;
 }
 
-function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+function Hello({ message, enthusiasmLevel = 1}: Props) {
   if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
 
   return (
-    <div className="hello">
-      <div className="greeting">
-        Hello {name + getExclamationMarks(enthusiasmLevel)}
-      </div>
-      <div>
-        <button onClick={onDecrement}>-</button>
-        <button onClick={onIncrement}>+</button>
-      </div>
+    <div>
+      {message + getExclamationMarks(enthusiasmLevel)} ;)
     </div>
   );
 }
