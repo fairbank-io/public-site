f12a72d23cc2a8b05328e7696be8048ce550c49f
Ben Cessa <ben@pixative.com>
2018-03-26 18:02:27 -0400

    Login Form

---
+++ src/home/LoginForm.tsx
@@ -0,0 +1,166 @@
+import * as React from 'react';
+import * as jQuery from 'jquery';
+import * as CryptoJS from 'crypto-js';
+import * as API from 'state/api';
+import { Session } from 'state/data';
+
+// Form elements
+interface FormItem {
+  name: string;
+  value: string;
+}
+
+// Component properties
+interface ComponentProps {
+  onNewSession: (session: Session) => void;
+}
+
+// Component state
+interface ComponentState {
+  alert: string;
+  alertLevel: string;
+}
+
+class LoginForm extends React.Component<ComponentProps, ComponentState> {
+  form: HTMLFormElement | null;
+
+  client: API.Client;
+
+  commonErrors: object = {
+    'INVALID_CREDENTIALS': 'Verifica tus credenciales y vuelve a intentarlo'
+  };
+
+  constructor(props: ComponentProps) {
+    super(props);
+    this.client = new API.Client();
+    this.handleLogin = this.handleLogin.bind(this);
+    this.state = {
+      alert: '',
+      alertLevel: 'warning'
+    };
+  }
+
+  public render(): JSX.Element {
+    // Build alert message
+    let alert: JSX.Element | null = null;
+    if (this.state.alert) {
+      let alertType: string = 'alert alert-' + this.state.alertLevel;
+      alert = <div className={alertType}>{this.state.alert}</div>;
+    }
+
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <p>Proporciona las credenciales de acceso de tu cuenta para iniciar una nueva sesión.</p>
+            {alert}
+            <form className="needs-validation" noValidate={true} ref={(el) => this.form = el}>
+              <div className="form-group">
+                <label>Correo Electrónico</label>
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                      <span className="icon">
+                        <i className="fas fa-envelope" />
+                      </span>
+                    </div>
+                  </div>
+                  <input
+                    type="email"
+                    name="email"
+                    required={true}
+                    className="form-control form-control-lg"
+                  />
+                  <div className="invalid-feedback">Debes proporcionar una dirección valida.</div>
+                </div>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <div className="input-group-text">
+                      <span className="icon">
+                        <i className="fas fa-key" />
+                      </span>
+                    </div>
+                  </div>
+                  <input
+                    type="password"
+                    name="password"
+                    minLength={8}
+                    required={true}
+                    className="form-control form-control-lg"
+                  />
+                  <div className="invalid-feedback">El campo es obligatorio con un mínimo de 8 caracteres.</div>
+                </div>
+                <small className="form-text text-muted">Nunca compartas tus credenciales de acceso con nadie.</small>
+              </div>
+              <div className="form-buttons">
+                <button className="btn btn-lg submit" onClick={this.handleLogin}>Iniciar Sesión</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  private handleLogin(): void {
+    if (!this.form) {
+      return;
+    }
+
+    // Validate form
+    if (!this.form.checkValidity()) {
+      jQuery(this.form).addClass('was-validated');
+      this.setState({
+        alert: 'Los datos ingresados no son correctos',
+        alertLevel: 'danger'
+      });
+      return;
+    }
+
+    // Run API request
+    let req: API.RequestSession = this.getRequest();
+    this.client.Login( req, (r, e) => {
+      // Failed requests
+      if (e) {
+        this.setState({
+          alert: 'Error Interno: ' + e,
+          alertLevel: 'danger'
+        });
+        return;
+      }
+
+      // Bad results
+      if (r && !r.ok) {
+        this.setState({
+          alert: this.commonErrors[r.desc] || r.desc,
+          alertLevel: 'warning'
+        });
+        return;
+      }
+
+      // All good!
+      if (r && r.ok) {
+        this.props.onNewSession(r.data as Session);
+      }
+    });
+  }
+
+  // Pack form data into a valid session request
+  private getRequest(): API.RequestSession {
+    let req: API.RequestSession = {} as API.RequestSession;
+    if (!this.form) {
+      return req;
+    }
+
+    jQuery(this.form).serializeArray().forEach(function (k: FormItem) {
+      req[k.name] = k.value;
+    });
+    req.password = CryptoJS.SHA256(req.password).toString();
+    return req;
+  }
+}
+
+export default LoginForm;
--- src/state/api.ts
+++ src/state/api.ts
@@ -1,16 +1,18 @@
+import axios from 'axios';
+import { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 import {
   Session,
   Referral
 } from 'state/data';
 
 // Request's dispatched to the platform's backend
-interface APIRequest {
-  session: Session;
+interface Request {
+  session?: Session;
   data: object;
 }
 
 // Responses received from the platform's backend
-interface APIResponse {
+interface Response {
   ok: boolean;
   desc: string;
   data: object;
@@ -32,18 +34,65 @@ interface RequestAccountRegister {
   referral?: Referral;
 }
 
-// Dispatch a new referral invite
-interface RequestAccountInvite {
-  name: string;
-  email: string;
-  message: string;
+// Callback handler for API client methods
+type ClientCallback = (r: Response | null, error: string | null) => void;
+
+class Client {
+  handler: AxiosInstance;
+
+  constructor() {
+    this.handler = axios.create({
+      baseURL:   'http://localhost:5559',
+      timeout:   5000,
+      httpAgent: 'public-site'
+    });
+  }
+
+  public Ping(cb: ClientCallback) {
+    this.request('/ping', cb);
+  }
+
+  public Login(data: RequestSession, cb: ClientCallback) {
+    this.request('/session/start', cb, data);
+  }
+
+  public Logout(session: Session, cb: ClientCallback) {
+    this.request('/session/end', cb, null, session);
+  }
+
+  public Register(data: RequestAccountRegister, cb: ClientCallback) {
+    this.request('/account', cb, data);
+  }
+
+  private request(path: string, cb: ClientCallback, data?: object | null, session?: Session) {
+    // Build API request
+    let req: Request = {} as Request;
+    if (data) {
+      req.data = data;
+    }
+    if (session) {
+      req.session = session;
+    }
+
+    // Execute request
+    this.handler.request({
+      method: 'post',
+      url:    path,
+      data:   req
+    }).then(function (r: AxiosResponse) {
+      cb(r.data as Response, null);
+    }).catch(function(error: AxiosError) {
+      cb(null, error.message);
+    });
+  }
 }
 
 // Module exports
 export {
-  APIRequest,
-  APIResponse,
+  Request,
+  Response,
+  Client,
+  ClientCallback,
   RequestSession,
-  RequestAccountInvite,
   RequestAccountRegister
 };
